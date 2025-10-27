b1e6a0e3b2fbe7c6a7664b34b66523827e62dc9f
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-10-27 18:51:16 +0100

    copied functions to c file

--- lists.c
+++ lists.c
@@ -1,5 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+lstelt_t *newtask(char *description) {}
+
+lstelt_t *newproj(char *description) {}
+
+lstelt_t *newgoal(char *description) {}
+
+int gettype(lstelt_t *element) {}
+
+int getid(lstelt_t *element) {}
+
+char *getdescr(lstelt_t *element) {}
+
+int *getchildren(lstelt_t *element) {}
--- lists.h
+++ lists.h
@@ -1,32 +1,28 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-enum elt_type {
-	TASK,
-	PROJECT,
-	GOAL
-};
+enum elt_type { TASK, PROJECT, GOAL };
 
 typedef struct lstelt lstelt_t; // list element
 struct lstelt {
-	char description[4048]; // or list name
-	elt_type type;
-	int identifier;
-	int * elements; // ptr to list of element, by id
+  char description[4048]; // or list name
+  enum elt_type type;
+  int identifier;
+  int *elements; // ptr to list of element, by id
 };
 
-task_t * newtask(char * description);
+lstelt_t *newtask(char *description);
 
-proj_t * newproj(char * description);
+lstelt_t *newproj(char *description);
 
-goal_t * newgoal(char * description);
+lstelt_t *newgoal(char *description);
 
-int gettype(lstelt_t * element);
+int gettype(lstelt_t *element);
 
-int getid(lstelt_t * element);
+int getid(lstelt_t *element);
 
-char * getdescr(lstelt_t * element);
+char *getdescr(lstelt_t *element);
 
-int * getchildren(lstelt_t * element);
+int *getchildren(lstelt_t *element);
 
-#endif //LISTS_H
+#endif // LISTS_H
