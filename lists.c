0143d2998cfd7d66357742ec8e2b55e22be4e1fb
Mouedh Souabni <mouedhsmsouabni@gmail.com>
2025-10-28 10:34:26 +0100

    Implemented constructors

--- lists.c
+++ lists.c
@@ -3,16 +3,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-lstelt_t *newtask(char *description) {}
+#define BUF_LEN 4048
 
-lstelt_t *newproj(char *description) {}
+lstelt_t *newtask(char *description) {
+  lstelt_t *ntask = malloc(BUF_LEN + sizeof(int) * 2);
+  strcpy(ntask->description, description);
+  ntask->type = TASK;
+  ntask->identifier = 1;
+  return ntask;
+}
 
-lstelt_t *newgoal(char *description) {}
+lstelt_t *newproj(char *description) {
+  lstelt_t *nproj = malloc(BUF_LEN + sizeof(int) * 2);
+  strcpy(nproj->description, description);
+  nproj->type = TASK;
+  nproj->identifier = 2;
+  return nproj;
+}
 
-int gettype(lstelt_t *element) {}
+lstelt_t *newgoal(char *description) {
+  lstelt_t *ngoal = malloc(BUF_LEN + sizeof(int) * 2);
+  strcpy(ngoal->description, description);
+  ngoal->type = TASK;
+  ngoal->identifier = 3;
+  return ngoal;
+}
 
-int getid(lstelt_t *element) {}
+int gettype(lstelt_t *element) { return element->type; }
 
-char *getdescr(lstelt_t *element) {}
+int getid(lstelt_t *element) { return 0; }
 
-int *getchildren(lstelt_t *element) {}
+char *getdescr(lstelt_t *element) { return element->description; }
+
+int *getchildren(lstelt_t *element) { return 0; }
